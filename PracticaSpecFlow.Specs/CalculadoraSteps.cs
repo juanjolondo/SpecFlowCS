34956a90f6d7b087bfaea1c663a8afc1023050e6
Juan José Londoño <33065960+juanjolondo@users.noreply.github.com>
2019-10-14 19:28:20 -0500

    Creación funcionalidad de sumar
    Se creó la funcionalidad de sumar, de acuerdo al turorial de SpecFlow.

---
+++ PracticaSpecFlow.Specs/CalculadoraSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using ProCalculadora;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PracticaSpecFlow.Specs
+{
+    [Binding]
+    public class CalculadoraSteps
+    {
+        private Calculadora calculadora = new Calculadora();
+        private int resultado;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int numero1)
+        {
+            calculadora.PrimerNumero = numero1;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int numero2)
+        {
+            calculadora.SegundoNumero = numero2;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            resultado = calculadora.Sumar();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int resultadoEsperado)
+        {
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+    }
+}
---
+++ ProCalculadora/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCalculadora
+{
+    public class Calculadora
+    {
+        public int PrimerNumero{ get; set; }
+        public int SegundoNumero { get; set; }
+
+        public int Sumar()
+        {
+            return PrimerNumero + SegundoNumero;
+        }
+    }
+}
